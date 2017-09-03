753f14d09119981f9b774b44b9df6da1550550a1
Joel <joel.flake@gmail.com>
2017-09-03 00:48:48 -0700

    Initial commit

---
+++ up.bat
@@ -0,0 +1,3 @@
+mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\ChronicleTesting.esp" "K:\FO4 Mods\Chronicle Testing\Data\ChronicleTesting.esp"
+mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\ChronicleTesting" "K:\FO4 Mods\Chronicle Testing\Data\Scripts\ChronicleTesting"
+mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\Source\User\ChronicleTesting" "K:\FO4 Mods\Chronicle Testing\Data\Scripts\Source\User\ChronicleTesting"
