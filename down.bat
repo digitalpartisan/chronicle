493b99026ed3b6c5abc00b5dfe3df6c5ec7f0ef9
digitalpartisan <digital.partisan.86@gmail.com>
2017-09-03 00:48:48 -0700

    Initial commit

---
+++ down.bat
@@ -0,0 +1,2 @@
+del /q "K:\Steam\steamapps\common\Fallout 4\Data\ChronicleTesting.esp"
+rmdir /s /q "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\ChronicleTesting" "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\Source\User\ChronicleTesting"
