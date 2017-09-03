2e262bcb8858c24bce1068914aeb1fd76f9166bc
digitalpartisan <digital.partisan.86@gmail.com>
2017-09-03 00:47:55 -0700

    Initial commit

---
+++ down-loose.bat
@@ -0,0 +1,1 @@
+rmdir /s /q "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\Chronicle" "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\Source\User\Chronicle"
---
+++ up-packed.bat
@@ -0,0 +1,1 @@
+mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\Chronicle - Main.ba2" "K:\FO4 Mods\Chronicle\Data\Chronicle - Main.ba2"
