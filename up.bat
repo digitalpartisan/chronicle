1661c40d799df3b1105922293f37459eadf40418
Joel <joel.flake@gmail.com>
2018-04-24 21:02:48 -0700

    Testing complete.  Added remote file for remote cases.

--- up.bat
+++ up.bat
@@ -1,3 +1,4 @@
 mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\ChronicleTesting.esp" "K:\FO4 Mods\Chronicle Testing\Data\ChronicleTesting.esp"
+mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\ChronicleTestingRemote.esp" "K:\FO4 Mods\Chronicle Testing\Data\ChronicleTestingRemote.esp"
 mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\ChronicleTesting" "K:\FO4 Mods\Chronicle Testing\Data\Scripts\ChronicleTesting"
 mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\Source\User\ChronicleTesting" "K:\FO4 Mods\Chronicle Testing\Data\Scripts\Source\User\ChronicleTesting"
